e40b79715618abcb62b50c7929dbe5e856e69435
unknown <peggy.gonzalez@upr.edu>
2023-10-23 00:18:39 -0400

    adding new methods but is not working, will try again in S3 folder

--- S2/main.go
+++ S2/main.go
@@ -1,15 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// card := OutCard()
 	// fmt.Println(card)
-	cards := 
+	cards := []deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
 }
 
-func OutCard() string {
+func newCard() string {
 	return "Five of Diamonds"
 }
