cda88deb47b9073d1eb4f5020f7b5aa584037215
kiki7766 <peggy.gonzalez@upr.edu>
2024-02-04 21:25:27 -0400

    Adding again more files and tests

--- S1/main.go
+++ S1/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi There!")
+	fmt.Println("Hi there!")
+
 }
+
+// Go Run - Compiles and execute program
+// Go Build - Only Compiles and creates an executable extra file
---
+++ S3/Proj1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades" //:= first initialization defining one time
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
